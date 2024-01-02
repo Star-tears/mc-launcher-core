6986d6068b69f7d6f2a5f44299cad7a4d5c08dba
Star-tears <1370443225@qq.com>
2024-01-02 21:46:12 +0800

    improve helper

--- src/types/shared_types.rs
+++ src/types/shared_types.rs
@@ -1,28 +1,28 @@
 use serde::Deserialize;
 use std::collections::HashMap;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ClientJsonRule {
     pub action: String,
     pub os: Option<HashMap<String, String>>,
     pub features: Option<HashMap<String, bool>>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[serde(untagged)]
 pub enum StringAndVecStringValue {
     StringValue(String),
     VecStringValue(Vec<String>),
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ClientJsonArgumentRule {
     pub compatibility_rules: Option<Vec<ClientJsonRule>>,
     pub rules: Option<Vec<ClientJsonRule>>,
     pub value: Option<StringAndVecStringValue>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ClientJsonAssetIndex {
     pub id: String,
     pub sha1: String,
@@ -32,21 +32,21 @@ pub struct ClientJsonAssetIndex {
     pub url: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ClientJsonDownloads {
     pub sha1: String,
     pub size: i32,
     pub url: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ClientJsonJavaVersion {
     pub component: String,
     #[serde(rename = "majorVersion")]
     pub major_version: i32,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ClientJsonLibraryDownloadsArtifact {
     pub path: String,
     pub url: String,
@@ -54,13 +54,13 @@ pub struct ClientJsonLibraryDownloadsArtifact {
     pub size: i32,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ClientJsonLibraryDownloads {
     pub artifact: Option<ClientJsonLibraryDownloadsArtifact>,
     pub classifiers: Option<HashMap<String, ClientJsonLibraryDownloadsArtifact>>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct ClientJsonLibrary {
     pub name: String,
     pub downloads: Option<ClientJsonLibraryDownloads>,
@@ -85,6 +85,13 @@ pub struct ClientJsonLogging {
     pub r#type: String,
 }
 
+#[derive(Debug, Deserialize, Clone)]
+#[serde(untagged)]
+pub enum StringAndClientJsonArgumentRuleValue {
+    StringValue(String),
+    ClientJsonArgumentRuleValue(ClientJsonArgumentRule),
+}
+
 #[derive(Debug, Deserialize)]
 #[serde(untagged)]
 pub enum StringAndClientJsonArgumentRuleValue {
@@ -95,30 +102,30 @@ pub enum StringAndClientJsonArgumentRuleValue {
 #[derive(Debug, Deserialize)]
 pub struct ClientJson {
     pub id: String,
-    pub jar: Option<String>,
-    pub arguments: HashMap<String, Vec<StringAndClientJsonArgumentRuleValue>>,
+    pub assets: String,
     #[serde(rename = "minecraftArguments")]
     pub minecraft_arguments: Option<String>,
-    #[serde(rename = "assetIndex")]
-    pub asset_index: ClientJsonAssetIndex,
-    pub assets: String,
-    pub downloads: HashMap<String, ClientJsonDownloads>,
-    #[serde(rename = "javaVersion")]
-    pub java_version: ClientJsonJavaVersion,
-    pub libraries: Vec<ClientJsonLibrary>,
-    pub logging: HashMap<String, ClientJsonLogging>,
-    #[serde(rename = "mainClass")]
-    pub main_class: String,
+    pub time: String,
     #[serde(rename = "minimumLauncherVersion")]
     pub minimum_launcher_version: i32,
+    pub jar: Option<String>,
+    #[serde(rename = "mainClass")]
+    pub main_class: String,
     #[serde(rename = "releaseTime")]
     pub release_time: String,
-    pub time: String,
     pub r#type: String,
-    #[serde(rename = "complianceLevel")]
-    pub compliance_level: i32,
     #[serde(rename = "inheritsFrom")]
     pub inherits_from: Option<String>,
+    pub libraries: Vec<ClientJsonLibrary>,
+    pub arguments: Option<HashMap<String, Vec<StringAndClientJsonArgumentRuleValue>>>,
+    #[serde(rename = "assetIndex")]
+    pub asset_index: Option<ClientJsonAssetIndex>,
+    pub downloads: Option<HashMap<String, ClientJsonDownloads>>,
+    #[serde(rename = "javaVersion")]
+    pub java_version: Option<ClientJsonJavaVersion>,
+    pub logging: Option<HashMap<String, ClientJsonLogging>>,
+    #[serde(rename = "complianceLevel")]
+    pub compliance_level: Option<i32>,
 }
 
 // need same as json
@@ -139,4 +146,50 @@ pub struct VersionListManifestJsonVersion {
 pub struct VersionListManifestJson {
     pub latest: HashMap<String, String>,
     pub versions: Vec<VersionListManifestJsonVersion>,
+}
+
+// impl
+impl ClientJson {
+    pub fn merge(&mut self, other: &ClientJson) {
+        self.id = other.id.clone();
+        self.assets = other.assets.clone();
+        if let Some(minecraft_arguments) = &other.minecraft_arguments {
+            self.minecraft_arguments = Some(minecraft_arguments.clone());
+        }
+        self.time = other.time.clone();
+        self.minimum_launcher_version = other.minimum_launcher_version.clone();
+        if let Some(jar) = &other.jar {
+            self.jar = Some(jar.clone());
+        }
+        self.main_class = other.main_class.clone();
+        self.release_time = other.release_time.clone();
+        self.r#type = other.r#type.clone();
+        if let Some(inherits_from) = &other.inherits_from {
+            self.inherits_from = Some(inherits_from.clone());
+        }
+        self.libraries.extend_from_slice(&other.libraries);
+
+        if let Some(arguments) = &other.arguments {
+            if let Some(self_arguments) = &mut self.arguments {
+                for (key, value) in arguments.iter() {
+                    if let Some(self_value) = self_arguments.get_mut(key) {
+                        self_value.extend_from_slice(value);
+                    } else {
+                        self_arguments.insert(key.clone(), value.clone());
+                    }
+                }
+            } else {
+                self.arguments = Some(arguments.clone());
+            }
+        }
+        if let Some(asset_index) = &other.asset_index {
+            self.asset_index = Some(asset_index.clone());
+        }
+        if let Some(java_version) = &other.java_version {
+            self.java_version = Some(java_version.clone());
+        }
+        if let Some(compliance_level) = &other.compliance_level {
+            self.compliance_level = Some(compliance_level.clone());
+        }
+    }
 }
--- src/utils/helper.rs
+++ src/utils/helper.rs
@@ -1,9 +1,248 @@
 use chrono::Utc;
+use crypto::digest::Digest;
+use crypto::sha1::Sha1;
 use lazy_static::lazy_static;
-use serde_json::Value;
-use std::{collections::HashMap, sync::Mutex};
+use regex::Regex;
+use reqwest::blocking::{Client, Response};
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::{self, BufWriter, Read},
+    path::Path,
+    sync::Mutex,
+};
+use sysinfo::System;
+use winver::WindowsVersion;
+use xz2::read::XzDecoder;
 
-use crate::types::helper_types::RequestsResponseCache;
+use crate::types::{
+    exceptions_types::InvalidChecksum,
+    helper_types::{MavenMetadata, RequestsResponseCache},
+    shared_types::{ClientJson, ClientJsonRule},
+    CallbackDict, MinecraftOptions,
+};
+
+pub fn check_path_inside_minecraft_directory(
+    minecraft_directory: impl AsRef<Path>,
+    path: impl AsRef<Path>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let minecraft_directory = minecraft_directory.as_ref().canonicalize().unwrap();
+    let path = path.as_ref().canonicalize().unwrap();
+
+    if !path.starts_with(&minecraft_directory) {
+        return Err(format!(
+            "{} is outside Minecraft directory {}",
+            path.to_string_lossy(),
+            minecraft_directory.to_string_lossy()
+        )
+        .into());
+    }
+    Ok(())
+}
+
+pub fn download_file(
+    url: &str,
+    path: &str,
+    sha1: Option<&str>,
+    lzma_compressed: bool,
+    minecraft_directory: Option<&str>,
+    session: Option<reqwest::blocking::Client>,
+    callback: CallbackDict,
+) -> Result<bool, Box<dyn std::error::Error>> {
+    if let Some(mc_dir) = minecraft_directory {
+        check_path_inside_minecraft_directory(mc_dir, path)?;
+    }
+
+    if Path::new(path).is_file() {
+        match sha1 {
+            Some(expected_sha1) => {
+                let actual_sha1 = get_sha1_hash(path)?;
+                if actual_sha1 == expected_sha1 {
+                    return Ok(false);
+                }
+            }
+            None => return Ok(false),
+        }
+    }
+
+    if let Some(parent_dir) = Path::new(path).parent() {
+        if let Err(err) = fs::create_dir_all(parent_dir) {
+            eprintln!("Error creating directories: {}", err);
+        }
+    }
+
+    if let Some(set_status) = callback.set_status {
+        if let Some(file_name) = Path::new(path).file_name() {
+            if let Some(file_name_str) = file_name.to_str() {
+                set_status(format!("Download {}", file_name_str));
+            }
+        }
+    }
+    let mut response: Response;
+    if session.is_none() {
+        let client = Client::builder()
+            .user_agent(get_user_agent()) // 设置 User-Agent
+            .build()?;
+        response = client.get(url).send()?;
+    } else {
+        response = Client::new().get(url).send()?;
+    }
+
+    if !response.status().is_success() {
+        return Ok(false);
+    }
+    let mut file = BufWriter::new(File::create(path)?);
+    if lzma_compressed {
+        let mut decoder = XzDecoder::new(response);
+        io::copy(&mut decoder, &mut file)?;
+    } else {
+        io::copy(&mut response, &mut file)?;
+    }
+
+    if let Some(expected_sha1) = sha1 {
+        let actual_sha1 = get_sha1_hash(path)?;
+        if actual_sha1 != expected_sha1 {
+            return Err(Box::new(InvalidChecksum {
+                url: url.to_string(),
+                path: path.to_string(),
+                expected: expected_sha1.to_string(),
+                actual: actual_sha1,
+            }));
+        }
+    }
+
+    Ok(true)
+}
+
+fn parse_single_rule(rule: &ClientJsonRule, options: &MinecraftOptions) -> bool {
+    let mut return_value = false;
+
+    if rule.action == "allow" {
+        return_value = false;
+    } else if rule.action == "disallow" {
+        return_value = true;
+    }
+
+    if let Some(os) = &rule.os {
+        if let Some(name) = os.get("name") {
+            if name == "windows" && std::env::consts::OS != "windows" {
+                return return_value;
+            } else if name == "osx" && std::env::consts::OS != "macos" {
+                return return_value;
+            } else if name == "linux" && std::env::consts::OS != "linux" {
+                return return_value;
+            }
+        }
+        if let Some(arch) = os.get("arch") {
+            if let Some(arch_info) = System::cpu_arch() {
+                if arch == "x86" && arch_info != "x86" {
+                    return return_value;
+                }
+            }
+        }
+        if let Some(version) = os.get("version") {
+            let os_version = get_os_version();
+            let re = Regex::new(version).unwrap();
+            if !re.is_match(&os_version) {
+                return return_value;
+            }
+        }
+    }
+
+    if let Some(features) = &rule.features {
+        if let Some(_) = features.get("has_custom_resolution") {
+            if !options.custom_resolution.unwrap_or(false) {
+                return return_value;
+            }
+        }
+        if let Some(_) = features.get("is_demo_user") {
+            if !options.demo.unwrap_or(false) {
+                return return_value;
+            }
+        }
+        if let Some(_) = features.get("has_quick_plays_support") {
+            if options.quick_play_path.is_none() {
+                return return_value;
+            }
+        }
+        if let Some(_) = features.get("is_quick_play_singleplayer") {
+            if options.quick_play_singleplayer.is_none() {
+                return return_value;
+            }
+        }
+        if let Some(_) = features.get("is_quick_play_multiplayer") {
+            if options.quick_play_multiplayer.is_none() {
+                return return_value;
+            }
+        }
+        if let Some(_) = features.get("is_quick_play_realms") {
+            if options.quick_play_realms.is_none() {
+                return return_value;
+            }
+        }
+    }
+
+    !return_value
+}
+
+pub fn parse_rule_list(rules: Vec<ClientJsonRule>, options: MinecraftOptions) -> bool {
+    for i in rules {
+        if !parse_single_rule(&i, &options) {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn inherit_json(
+    original_data: &ClientJson,
+    path: impl AsRef<Path>,
+) -> Result<ClientJson, Box<dyn std::error::Error>> {
+    let inherit_version = original_data
+        .inherits_from
+        .as_ref()
+        .ok_or("Missing 'inheritsFrom' key")?;
+
+    let mut file_path = path.as_ref().canonicalize()?;
+    file_path.push("versions");
+    file_path.push(inherit_version);
+    file_path.push(format!("{}.json", inherit_version));
+
+    let mut file = File::open(file_path)?;
+    let mut file_content = String::new();
+    file.read_to_string(&mut file_content)?;
+
+    let mut new_data: ClientJson = serde_json::from_str(&file_content)?;
+
+    new_data.merge(original_data);
+
+    Ok(new_data)
+}
+
+pub fn get_sha1_hash(path: impl AsRef<Path>) -> Result<String, Box<dyn std::error::Error>> {
+    const BUF_SIZE: usize = 65536;
+    let mut file = File::open(path)?;
+    let mut buffer = [0; BUF_SIZE];
+    let mut sha1 = Sha1::new();
+
+    loop {
+        let bytes_read = file.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+        sha1.input(&buffer[..bytes_read]);
+    }
+
+    Ok(sha1.result_str())
+}
+
+pub fn get_os_version() -> String {
+    if std::env::consts::OS == "windows" {
+        let version = WindowsVersion::detect().unwrap();
+        return format!("{}.{}", version.major, version.minor);
+    }
+    System::os_version().expect("failed get os version")
+}
 
 lazy_static! {
     static ref REQUESTS_RESPONSE_CACHE: Mutex<HashMap<String, RequestsResponseCache>> =
@@ -23,12 +262,12 @@ pub fn get_user_agent() -> String {
     }
 }
 
-pub fn get_requests_response_cache(url: &str) -> Result<Value, reqwest::Error> {
+pub fn get_requests_response_cache(url: &str) -> Result<String, reqwest::Error> {
     let mut cache = REQUESTS_RESPONSE_CACHE.lock().unwrap();
     if let Some(cache_entry) = cache.get(url) {
         let elapsed = Utc::now() - cache_entry.datetime;
         if elapsed.num_seconds() > 3600 {
-            let response: Value = reqwest::blocking::get(url)?.json()?;
+            let response = reqwest::blocking::get(url)?.text()?;
             let res = response.clone();
             let cache_entry = RequestsResponseCache {
                 response,
@@ -39,7 +278,7 @@ pub fn get_requests_response_cache(url: &str) -> Result<Value, reqwest::Error> {
         }
     }
 
-    let response: Value = reqwest::blocking::get(url)?.json()?;
+    let response = reqwest::blocking::get(url)?.text()?;
     let res = response.clone();
     let cache_entry = RequestsResponseCache {
         response,
@@ -49,20 +288,68 @@ pub fn get_requests_response_cache(url: &str) -> Result<Value, reqwest::Error> {
     Ok(res)
 }
 
+pub fn parse_maven_metadata(url: &str) -> Result<MavenMetadata, Box<dyn std::error::Error>> {
+    let response = get_requests_response_cache(url)?;
+
+    let release_regex = Regex::new(r#"<release>(.*?)</release>"#)?;
+    let latest_regex = Regex::new(r#"<latest>(.*?)</latest>"#)?;
+    let version_regex = Regex::new(r#"<version>(.*?)</version>"#)?;
+
+    let release = release_regex
+        .captures(&response)
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str()
+        .to_string();
+    let latest = latest_regex
+        .captures(&response)
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str()
+        .to_string();
+    let versions: Vec<String> = version_regex
+        .captures_iter(&response)
+        .map(|m| m.get(1).unwrap().as_str().to_string())
+        .collect();
+
+    Ok(MavenMetadata {
+        release,
+        latest,
+        versions,
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_get_user_agent() {
+    fn debug_get_user_agent() {
         let user_agent = get_user_agent();
         println!("Now user_agent: {:?}", user_agent);
     }
 
     #[test]
-    fn test_get_requests_response_cache() {
+    fn debug_get_requests_response_cache() {
         if let Ok(response) = get_requests_response_cache("https://httpbin.org/ip") {
             println!("{:#?}", response);
         }
+    }
+
+    #[test]
+    fn debug_parse_maven_metadata() {
+        // let url =
+        //     "https://files.minecraftforge.net/maven/net/minecraftforge/forge/maven-metadata.xml";
+        // match parse_maven_metadata(url) {
+        //     Ok(res) => println!("{:?}", res),
+        //     Err(e) => println!("{:#?}", e),
+        // }
+    }
+
+    #[test]
+    fn debug_get_os_version() {
+        println!("{}", get_os_version());
     }
 }
