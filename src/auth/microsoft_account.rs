309ddfa21a15d7feec643978c5153a3c4b7ed8c7
Star-tears <1370443225@qq.com>
2024-01-12 13:28:36 +0800

    add fn generate_pkce_data

--- src/auth/microsoft_account.rs
+++ src/auth/microsoft_account.rs
@@ -1,3 +1,6 @@
+use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine};
+use rand::{distributions::Alphanumeric, Rng};
+use ring::digest;
 use std::collections::HashMap;
 use url::Url;
 
@@ -19,4 +22,53 @@ pub fn get_login_url(client_id: &str, redirect_uri: &str) -> String {
         .expect("Failed to build URL");
 
     url_with_query.to_string()
+}
+
+fn generate_pkce_data() -> (String, String, String) {
+    let mut rng = rand::thread_rng();
+    let chars: Vec<char> = (0..128)
+        .map(|_| match rng.gen_range(0..64) {
+            0 => '-',
+            1 => '_',
+            _ => rng.sample(Alphanumeric) as char,
+        })
+        .collect();
+    let code_verifier: String = chars.iter().collect();
+
+    let digest = digest::digest(&digest::SHA256, code_verifier.as_bytes());
+    let code_challenge = URL_SAFE_NO_PAD.encode(digest.as_ref());
+    code_challenge.trim_end_matches('=').to_string();
+    let code_challenge_method = "S256".to_string();
+
+    (code_verifier, code_challenge, code_challenge_method)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_code_challenge() {
+        let code_verifier: String = "7BSNrJnbWnVrx9Y3uoBEJmrd0eii9ZBEQ5AVw_j4lzIlnsxwTDLJdtaiuCdrkJZ4fVH-E3v_hP7ynwS4zIwrSVCzG7vr5MTXahwESJnsb3SFM5zpdNjj525JbjrUwctt".to_string();
+        let digest = digest::digest(&digest::SHA256, code_verifier.as_bytes());
+        let code_challenge = URL_SAFE_NO_PAD.encode(digest.as_ref());
+        code_challenge.trim_end_matches('=').to_string();
+        assert_eq!(
+            code_challenge,
+            "bOQuaNvcR9utb6HhxpkDuvJr4Wh83ugr_FnH4dvTg9I".to_string()
+        );
+        let code_verifier: String = "sL0L64E7Qk_TANBue-ejOajO7LP3dcVI64ZgsjMsfV5dMhuDoFgb0Ldb4b7U3EXqBldbZJEAMJoxE8NfFmvm2oimm2FDQhy2qPDEoWUsY60mXF1poaw5cwvnpK-dXSFB".to_string();
+        let digest = digest::digest(&digest::SHA256, code_verifier.as_bytes());
+        let code_challenge = URL_SAFE_NO_PAD.encode(digest.as_ref());
+        code_challenge.trim_end_matches('=').to_string();
+        assert_eq!(
+            code_challenge,
+            "Nju8uPgZTErU1OxovBkfsGwykuhtCVCE-dGGhooiD8E".to_string()
+        );
+    }
+
+    #[test]
+    fn debug_generate_pkce_data() {
+        println!("{:#?}", generate_pkce_data());
+    }
 }
