94a181975663d3b0a5735ea9d1999ba5af7a0e6e
Star-tears <1370443225@qq.com>
2024-01-20 10:47:28 +0800

    add fn get_data_library_path

--- src/forge/mod.rs
+++ src/forge/mod.rs
@@ -1,3 +1,27 @@
+use std::path::Path;
+
+fn get_data_library_path(libname: &str, path: impl AsRef<Path>) -> String {
+    let libname = &libname[1..libname.len() - 1];
+    let mut libpath = path.as_ref().join("libraries");
+    let mut parts = libname.split(":");
+    let base_path = parts.next().unwrap_or_default();
+    let libname = parts.next().unwrap_or_default();
+    let version = parts.next().unwrap_or_default();
+    let mut extra = parts.next().unwrap_or_default();
+    for i in base_path.split("."){
+        libpath.push(i);
+    }
+    let mut fileend = "jar";
+    if let Some(at_index) = extra.find('@') {
+        fileend = &extra[at_index + 1..];
+        extra = &extra[..at_index];
+    }
+    libpath.push(libname);
+    libpath.push(version);
+    libpath.push(format!("{}-{}-{}.{}", libname, version, extra, fileend));
+    libpath.to_str().unwrap_or_default().to_owned()
+}
+
 pub fn forge_to_installed_version(forge_version: &str) -> Result<String, String> {
     match forge_version.split_once("-") {
         Some((vanilla_part, forge_part)) => Ok(format!("{}-forge-{}", vanilla_part, forge_part)),
