dfe42f8a46b4a6f5c1ce9cd2351eccf51e2cc585
Star-tears <star-tears@outlook.com>
2026-05-27 20:18:44 +0800

    docs: update launcher facade examples

--- README.md
+++ README.md
@@ -10,51 +10,45 @@ Use `cargo add mc-launcher-core` to add it to your project.
 
 ## Example
 
-Here's an example of launching the latest version of mc offline:
+Here's an example of installing Fabric and launching offline:
 
 ```rust
 use std::process::Command;
 
-use mc_launcher_core::{
-    auth::offline::get_offline_options,
-    command::get_minecraft_command,
-    install::install_minecraft_version,
-    types::CallbackDict,
-    utils::{get_latest_version, get_minecraft_directory},
-};
+use mc_launcher_core::prelude::*;
 
-fn main() {
-    let options = get_offline_options("Steve");
-    let latest_version = get_latest_version().unwrap().release;
-    let minecraft_directory = get_minecraft_directory();
+fn main() -> mc_launcher_core::Result<()> {
+    let minecraft_dir = mc_launcher_core::utils::get_minecraft_directory();
+    let launcher = Launcher::new(minecraft_dir);
 
-    let _ = install_minecraft_version(
-        &latest_version,
-        &minecraft_directory,
-        &CallbackDict::default(),
-    );
+    let install = launcher.install(InstallRequest {
+        minecraft_version: "1.20.4".to_string(),
+        loader: Some(LoaderSpec::Fabric {
+            version: LoaderVersion::LatestStable,
+        }),
+        java: JavaInstallPolicy::Auto,
+    })?;
 
-    let minecraft_command_result = get_minecraft_command(
-        &get_latest_version().unwrap().release,
-        &minecraft_directory,
-        &options,
-    );
+    let version_json_path = launcher
+        .minecraft_dir()
+        .join("versions")
+        .join(&install.version_id)
+        .join(format!("{}.json", install.version_id));
+    let version_json: mc_launcher_core::core::version::VersionJson =
+        serde_json::from_str(&std::fs::read_to_string(version_json_path)?)?;
 
-    match minecraft_command_result {
-        Ok(minecraft_command) => {
-            // Start Minecraft
-            let mut cmd = Command::new(&minecraft_command[0])
-                .args(&minecraft_command[1..minecraft_command.len()])
-                .spawn()
-                .expect("Failed to start Minecraft");
-            let _ = cmd.wait();
-        }
-        Err(err) => {
-            eprintln!("Failed to get Minecraft command: {}", err);
-        }
-    }
+    let command = launcher.build_launch_command_from_version(
+        &version_json,
+        LaunchOptions {
+            account: Account::offline("Steve"),
+            ..Default::default()
+        },
+    )?;
+
+    let mut child = Command::new(command.executable).args(command.args).spawn()?;
+    child.wait()?;
+    Ok(())
 }
-
 ```
 
 ## Todo list
@@ -63,7 +57,7 @@ fn main() {
 - [x] Install original version
 - [x] Offline launch
 - [x] Microsoft account login
-- [ ] Support for Forge, Fabric, Quilt, and Liteloader
+- [x] Support for Forge, Fabric, Quilt, and NeoForge loader metadata
 - [ ] Install of mrpack modpacks
 - [ ] Comprehensive documentation
 
--- examples/simple_launch.rs
+++ examples/simple_launch.rs
@@ -1,65 +1,38 @@
-use std::{io, process::Command};
+use std::process::Command;
 
-use mc_launcher_core::{
-    auth::microsoft_account::{complete_login, get_secure_login_data, parse_auth_code_url},
-    command::get_minecraft_command,
-    install::install_minecraft_version,
-    types::{CallbackDict, MinecraftOptions},
-    utils::{get_latest_version, get_minecraft_directory},
-};
+use mc_launcher_core::prelude::*;
 
-// Set the data for your Azure Application here.
-const CLIENT_ID: &str = "YOUR CLIENT ID";
-const REDIRECT_URI: &str = "YOUR REDIRECT URL";
+fn main() -> mc_launcher_core::Result<()> {
+    let minecraft_dir = mc_launcher_core::utils::get_minecraft_directory();
+    let launcher = Launcher::new(minecraft_dir);
 
-fn main() {
-    let latest_version = get_latest_version().unwrap().release;
-    let minecraft_directory = get_minecraft_directory();
+    let install = launcher.install(InstallRequest {
+        minecraft_version: "1.20.4".to_string(),
+        loader: Some(LoaderSpec::Fabric {
+            version: LoaderVersion::LatestStable,
+        }),
+        java: JavaInstallPolicy::Auto,
+    })?;
 
-    let _ = install_minecraft_version(
-        &latest_version,
-        &minecraft_directory,
-        &CallbackDict::default(),
-    );
+    let version_json_path = launcher
+        .minecraft_dir()
+        .join("versions")
+        .join(&install.version_id)
+        .join(format!("{}.json", install.version_id));
+    let version_json: mc_launcher_core::core::version::VersionJson =
+        serde_json::from_str(&std::fs::read_to_string(version_json_path)?)?;
 
-    let (login_url, state, code_verifier) = get_secure_login_data(CLIENT_ID, REDIRECT_URI, None);
-    println!(
-        "Please open {} in your browser and copy the url you are redirected into the prompt below.",
-        login_url
-    );
-    let mut code_url = String::new();
-    io::stdin().read_line(&mut code_url).expect("input error");
+    let command = launcher.build_launch_command_from_version(
+        &version_json,
+        LaunchOptions {
+            account: Account::offline("Steve"),
+            ..Default::default()
+        },
+    )?;
 
-    let auth_code = parse_auth_code_url(&code_url, Some(state)).expect("Error get auth_code");
-
-    let login_data = complete_login(
-        CLIENT_ID,
-        None,
-        REDIRECT_URI,
-        &auth_code,
-        Some(&code_verifier),
-    )
-    .unwrap();
-
-    let options = MinecraftOptions::new(login_data.name, login_data.id, login_data.access_token);
-
-    let minecraft_command_result = get_minecraft_command(
-        &get_latest_version().unwrap().release,
-        &minecraft_directory,
-        &options,
-    );
-
-    match minecraft_command_result {
-        Ok(minecraft_command) => {
-            // Start Minecraft
-            let mut cmd = Command::new(&minecraft_command[0])
-                .args(&minecraft_command[1..minecraft_command.len()])
-                .spawn()
-                .expect("Failed to start Minecraft");
-            let _ = cmd.wait();
-        }
-        Err(err) => {
-            eprintln!("Failed to get Minecraft command: {}", err);
-        }
-    }
+    let mut child = Command::new(command.executable)
+        .args(command.args)
+        .spawn()?;
+    child.wait()?;
+    Ok(())
 }
